f5d0387a16d52a3fc43c0ddfa9a7fd74c1872f30
Connie Leung <cleung334@gmail.com>
2025-05-18 21:49:00 +0800

    feat: add CoffeePlan component

---
+++ src/lib/coffee-plan.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<div class="plan">
+    <div class="description">
+        <span class="title"> The Single </span>
+    </div>
+</div>
+
+<style></style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,33 @@
+<script lang="ts">
+    import CoffeePlan from '$lib/coffee-plan.svelte';
+</script>
+
 <div class="content">
 	<h1 class="title">Coffee Plans</h1>
 
 	<h2 class="subtitle">We travel the world to source the very best single origin coffee for you</h2>
 
 	<div class="plans">
-		<div class="plan">
+        <CoffeePlan />
+        <CoffeePlan />
+        <CoffeePlan />
+        <CoffeePlan />
+        <!-- <div class="plan">
 			<div class="description">
 				<span class="title"> The Single </span>
 			</div>
-		</div>
+		</div> -->
 
-		<div class="plan">
+		<!-- <div class="plan">
 			<div class="description">
 				<span class="title"> The Curious </span>
 			</div>
-		</div>
+		</div> -->
 
-		<div class="plan">
+		<!-- <div class="plan">
 			<div class="description">
 				<span class="title"> The Addict </span>
 			</div>
-		</div>
+		</div> -->
 	</div>
 </div>

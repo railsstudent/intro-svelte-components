9fb1077e1743ce206b1242aa0706afa94e71bacd
Connie Leung <cleung334@gmail.com>
2025-05-18 20:02:19 +0800

    chore: starter codes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="content">
+	<h1 class="title">Coffee Plans</h1>
+
+	<h2 class="subtitle">We travel the world to source the very best single origin coffee for you</h2>
+
+	<div class="plans">
+		<div class="plan">
+			<div class="description">
+				<span class="title"> The Single </span>
+			</div>
+		</div>
+
+		<div class="plan">
+			<div class="description">
+				<span class="title"> The Curious </span>
+			</div>
+		</div>
+
+		<div class="plan">
+			<div class="description">
+				<span class="title"> The Addict </span>
+			</div>
+		</div>
+	</div>
+</div>

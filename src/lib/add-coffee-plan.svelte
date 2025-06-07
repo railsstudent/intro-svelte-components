aec37ce087a82d64e8d762c70fd4ef47009da6f9
Connie Leung <cleung334@gmail.com>
2025-06-07 08:02:47 +0800

    feat: use snippet to update add plan button text. The snippet is an implicit prop of the component.

--- src/lib/add-coffee-plan.svelte
+++ src/lib/add-coffee-plan.svelte
@@ -1,51 +1,62 @@
 <script lang="ts">
-interface Props {
-    addCoffeePlan: (plan: string) => void;
-}
+	import type { Snippet } from 'svelte';
 
-const { addCoffeePlan }: Props = $props();
+	interface Props {
+		addCoffeePlan: (plan: string) => void;
+		addPlanButton: Snippet<[boolean]>;
+	}
 
-let newPlan = $state('');
-const addPlan = (e: SubmitEvent) => {
-    e.preventDefault();
-    const trimmedPlan = newPlan.trim();
-    if (!trimmedPlan) {
-        return;
-    }
-    
-    addCoffeePlan(trimmedPlan);
-    newPlan = '';
-}
+	const { addCoffeePlan, addPlanButton }: Props = $props();
+
+	let newPlan = $state('');
+	const addPlan = (e: SubmitEvent) => {
+		e.preventDefault();
+		const trimmedPlan = newPlan.trim();
+		if (!trimmedPlan) {
+			return;
+		}
+
+		addCoffeePlan(trimmedPlan);
+		newPlan = '';
+	};
+
+	let hover = $state(false);
 </script>
 
 <form class="add-plan-form" onsubmit={addPlan}>
-    <input type="text" placeholder="Add a new plan" bind:value={newPlan} />
-    <button type="submit" class="btn btn-primary" disabled={newPlan.length < 5}>
-        Add Plan
-    </button>
-</form>	
+	<input type="text" placeholder="Add a new plan" bind:value={newPlan} />
+	<button
+		type="submit"
+		class="btn btn-primary"
+		disabled={newPlan.length < 5}
+		onmouseenter={() => (hover = true)}
+		onmouseleave={() => (hover = false)}
+	>
+		{@render addPlanButton(hover)}
+	</button>
+</form>
 
 <style>
-    input {
-      padding: 0.5rem 0.75rem;
-    }
-    
-    .add-plan-form {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-    }
-    
-    .add-plan-form input {
-      width: 70%;
-      border-radius: 3px;
-      box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
-      border: 1px solid #f1f5f8;
-      color: #606f7b;
-      padding: 0.5rem 0.75rem;
-      box-sizing: border-box;
-      font-size: 1rem;
-      letter-spacing: 0.5px;
-      margin: 0.5rem 0;
-    }
+	input {
+		padding: 0.5rem 0.75rem;
+	}
+
+	.add-plan-form {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+
+	.add-plan-form input {
+		width: 70%;
+		border-radius: 3px;
+		box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+		border: 1px solid #f1f5f8;
+		color: #606f7b;
+		padding: 0.5rem 0.75rem;
+		box-sizing: border-box;
+		font-size: 1rem;
+		letter-spacing: 0.5px;
+		margin: 0.5rem 0;
+	}
 </style>
--- src/lib/plan-picker.svelte
+++ src/lib/plan-picker.svelte
@@ -4,11 +4,15 @@
 	const plans = $state(['The Single', 'The Curious', 'The Addict', 'The Hacker']);
 
 	let selectedCoffeePlan = $state('');
-	const selectedPlan = (name: string) => selectedCoffeePlan = name;
+	const selectedPlan = (name: string) => (selectedCoffeePlan = name);
 </script>
 
 <div class="plans">
-	<AddCoffeePlan addCoffeePlan={(plan) => plans.push(plan)}  />
+	<AddCoffeePlan addCoffeePlan={(plan) => plans.push(plan)}>
+		{#snippet addPlanButton(hover: boolean)}
+			Add Plan {hover ? '(+1)' : ''}
+		{/snippet}	
+	</AddCoffeePlan>
 	{selectedCoffeePlan}
 	{#each plans as plan (plan)}
 		<CoffeePlan name={plan} {selectedPlan} selected={selectedCoffeePlan === plan} />

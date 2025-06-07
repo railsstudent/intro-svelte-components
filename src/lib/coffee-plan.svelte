4e3500786a30f7e41ca7f366b47e0969b4fa43af
Connie Leung <cleung334@gmail.com>
2025-06-07 08:40:55 +0800

    feat:  pass icons snippet to component

--- src/lib/coffee-plan.svelte
+++ src/lib/coffee-plan.svelte
@@ -1,18 +1,24 @@
 <script lang="ts">
+	import type { Snippet } from 'svelte';
+
 	interface Props {
 		name: string;
 		selectedPlan: (name: string) => void;
 		selected: boolean;
+		selectedPlanIcons?: Snippet<[string]>;
 	}
 
-	let { name = 'Default Plan', selectedPlan, selected }: Props = $props();
+	let { name = 'Default Plan', selectedPlan, selected, selectedPlanIcons }: Props = $props();
 
 	const handleSelectPlan = () => {
 		selectedPlan(name);
 	};
 </script>
 
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_static_element_interactions -->
 <div onclick={handleSelectPlan} class={['plan', selected && 'active-plan']}>
+	{@render selectedPlanIcons?.(name)}
 	<div class="description">
 		<span class="title"> {name} </span>
 	</div>
--- src/lib/plan-picker.svelte
+++ src/lib/plan-picker.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+	import Icon from "@iconify/svelte";
+
 	import CoffeePlan from './coffee-plan.svelte';
 	import AddCoffeePlan from './add-coffee-plan.svelte';
 	const plans = $state(['The Single', 'The Curious', 'The Addict', 'The Hacker']);
 
 	let selectedCoffeePlan = $state('');
 	const selectedPlan = (name: string) => (selectedCoffeePlan = name);
+	const isSelected = (plan: string) => selectedCoffeePlan === plan;
 </script>
 
 <div class="plans">
@@ -14,7 +17,21 @@
 		{/snippet}	
 	</AddCoffeePlan>
 	{selectedCoffeePlan}
+
+	{#snippet selectedPlanIcons(name: string)}
+		<div>
+			{#if name.startsWith('The')}
+				<Icon icon="ic:outline-coffee" width="48" height="48" />
+				<Icon icon="ic:outline-coffee-maker" width="48" height="48" />
+			{:else}
+				<Icon icon="material-symbols:emoji-food-beverage-outline" width="48" height="48" />
+				<Icon icon="material-symbols:fastfood-outline" width="48" height="48" />
+			{/if}
+		</div>
+	{/snippet}
+
 	{#each plans as plan (plan)}
-		<CoffeePlan name={plan} {selectedPlan} selected={selectedCoffeePlan === plan} />
+		<CoffeePlan name={plan} {selectedPlan} selected={isSelected(plan)} 
+			selectedPlanIcons={isSelected(plan) ? selectedPlanIcons : undefined } />
 	{/each}
 </div>
